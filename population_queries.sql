ebb979fc08c101ec5514ca5a9f81fc433eb8a6d4
thismeansthat <60375483+thismeansthat@users.noreply.github.com>
2020-03-23 20:07:45 +1100

    Update population_queries.sql

--- population_queries.sql
+++ population_queries.sql
@@ -1,6 +1,24 @@
--- This is the first query:
-
 SELECT DISTINCT year from population_years;
 
--- Add your additional queries below:
+SELECT * FROM population_years
+WHERE country = 'Gabon'
+ORDER BY population DESC; 
 
+SELECT * FROM population_years
+WHERE year IS '2005'
+ORDER BY population ASC
+LIMIT 10;
+
+SELECT DISTINCT country
+FROM population_years
+WHERE population > '100'
+	AND year = '2010';
+
+SELECT DISTINCT country
+FROM population_years
+WHERE country LIKE '%Islands%';
+
+SELECT *
+FROM population_years
+WHERE country IS 'Indonesia'
+AND year BETWEEN '2000' AND '2010';
